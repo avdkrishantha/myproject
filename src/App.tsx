224addf32c220182730208da248518ab193bca4d
avdkrishantha <126566288+avdkrishantha@users.noreply.github.com>
2023-08-22 14:02:48 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <UserData />
+      <UserData />
+    </div>
+  );
+}
+// cons
+const UserData = () => {
+  return (
+    <div>
+      <h1> Saman</h1>
+      <p> saman@gmail.com</p>
+    </div>
+  );
+};
